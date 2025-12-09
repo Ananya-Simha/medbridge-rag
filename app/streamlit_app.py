70a8a9c08cbfbceffa8b37b48fc06f44467c72cb
Ananya Simha <ananyasimha06@gmail.com>
2025-12-08 23:02:25 -0800

    External IP updated

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://172.27.16.1:8000/answer" 
+API_URL = "http://34.73.47.149:8000/answer" 
 
 st.set_page_config(page_title="MedBridge", layout="wide")
 st.title("MedBridge: Patient-Friendly Medical Answers")
